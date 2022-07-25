21a1ee3d26a67642e1b73149e48f80cdae7da7d9
DESKTOP-R3OMP13\pc <wnsgus3132@naver.com>
2022-07-25 09:14:26 +0900

    남원, 곡성 일출 일몰 수정

--- src/main/java/com/tx/dyUser/wether/WetherService.java
+++ src/main/java/com/tx/dyUser/wether/WetherService.java
@@ -436,8 +436,8 @@ public class WetherService {
 		   if(region.equals("세종")) area = URLEncoder.encode("세종", "utf-8");
 		   if(region.equals("영암")) area = URLEncoder.encode("목포", "utf-8");
 		   if(region.equals("김제")) area = URLEncoder.encode("부안", "utf-8");
-		   if(region.equals("남원")) area = URLEncoder.encode("광주", "utf-8");
-		   if(region.equals("곡성")) area = URLEncoder.encode("광주", "utf-8");
+		   if(region.equals("남원")) area = URLEncoder.encode("남원", "utf-8");
+		   if(region.equals("곡성")) area = URLEncoder.encode("남원", "utf-8");
 
 		   String urlstr = "http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo?serviceKey=dQsCZl8ZlcJHAjjmit2miCTpY042aQYG2P%2Bbnq%2BuVToDqFAVoVv%2Bdx%2FUbDLF6RvjVqVdYHAw%2FGrlbMyCSbdbHA%3D%3D&locdate="+dates+"&location="+area;
 		   
